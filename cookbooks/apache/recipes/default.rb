ddebf7b5fdc6b1db4867066a87e9a06ab22634b1
sumit <sumit.salunke@afoutech.com>
2017-07-03 14:48:32 +0000

    Adding postgresql cookbook

--- cookbooks/apache/recipes/default.rb
+++ cookbooks/apache/recipes/default.rb
@@ -3,10 +3,10 @@
 # Recipe:: default
 #
 # Copyright:: 2017, The Authors, All Rights Reserved.
-if node[platform_family] == "rhel"
-	package='httpd'
-elseif node[platform_family] == "debian"
-	package='apache2'
+if node['platform_family'] == "rhel"
+	package="httpd"
+elsif node['platform_family'] == "debian"
+	package="apache2"
 end
 
 package 'apache2' do
